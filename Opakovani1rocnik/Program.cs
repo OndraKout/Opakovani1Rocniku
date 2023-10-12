11a8b5e6fb7733db9d157e974e4fe7a857891f39
Ondřej KOUT <kouton@delta-studenti.cz>
2023-10-12 14:19:34 +0200

    2. uložení

--- Opakovani1rocnik/Program.cs
+++ Opakovani1rocnik/Program.cs
@@ -6,12 +6,6 @@ namespace Opakovani1rocnik
     {
         static void Main(string[] args)
         {
-            Random skodanadrz = new Random();
-            Auto Škoda = new Auto();
-            Škoda.jménoauta = "Škoda";
-            Škoda.kilometrynalitr = 10;
-            Škoda.nádrž = 55;
-            Škoda.aktuálnínádrž = skodanadrz.Next(10, 55);
 
             Random ferarinadrz = new Random();
             Auto Ferari = new Auto();
@@ -20,13 +14,32 @@ namespace Opakovani1rocnik
             Ferari.nádrž = 80;
             Ferari.aktuálnínádrž = ferarinadrz.Next(10, 80);
 
-            string vyber = "";
-
-            while (vyber != "ukonceni") ;
-            Console.WriteLine("Zdravím, vyber si auto s kterým chceš jet na výlet");
+            int vyber = 0;
+            Console.WriteLine("Zdravím, vyber si auto s kterým chceš jet na výlet.(Zadej číslo)");
             Console.WriteLine("1.Škoda");
             Console.WriteLine("2.Ferrari");
-            Console.WriteLine("Pro specifikace zadej |specifikace| a pro u končení |ukonceni|");
+            vyber = int.Parse(Console.ReadLine());
+            Specifikace(vyber); 
+
+            while (true)
+            {
+            }
+        }
+
+        static public void Specifikace(int vyber)
+        {
+            if (vyber == 1)
+            {
+                Random skodanadrz = new Random();
+                Auto Škoda = new Auto();
+                Console.WriteLine(Škoda.jménoauta = "Škoda");
+                Console.WriteLine(Škoda.jménoauta);
+                Škoda.kilometrynalitr = 10;
+                Škoda.nádrž = 55;
+                Škoda.aktuálnínádrž = skodanadrz.Next(10, 55);
+            }
+
+
         }
     }
 }
