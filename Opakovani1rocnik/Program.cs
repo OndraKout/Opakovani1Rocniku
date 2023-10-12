11f59faa04ffb7f99f4b82ad48ad0da267bc32de
Ondřej KOUT <kouton@delta-studenti.cz>
2023-10-12 13:58:27 +0200

    Začátek programu

--- Opakovani1rocnik/Program.cs
+++ Opakovani1rocnik/Program.cs
@@ -6,7 +6,27 @@ namespace Opakovani1rocnik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random skodanadrz = new Random();
+            Auto Škoda = new Auto();
+            Škoda.jménoauta = "Škoda";
+            Škoda.kilometrynalitr = 10;
+            Škoda.nádrž = 55;
+            Škoda.aktuálnínádrž = skodanadrz.Next(10, 55);
+
+            Random ferarinadrz = new Random();
+            Auto Ferari = new Auto();
+            Ferari.jménoauta = "Škoda";
+            Ferari.kilometrynalitr = 10;
+            Ferari.nádrž = 80;
+            Ferari.aktuálnínádrž = ferarinadrz.Next(10, 80);
+
+            string vyber = "";
+
+            while (vyber != "ukonceni") ;
+            Console.WriteLine("Zdravím, vyber si auto s kterým chceš jet na výlet");
+            Console.WriteLine("1.Škoda");
+            Console.WriteLine("2.Ferrari");
+            Console.WriteLine("Pro specifikace zadej |specifikace| a pro u končení |ukonceni|");
         }
     }
 }
---
+++ Opakovani1rocnik/Vylet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opakovani1rocnik
+{
+    internal class Vylet
+    {
+        public int delkacesty;
+        static public string[] vylety = { "Výlet na hrad", "Výlet na koupák", "Výlet do Zoo", "Výlet do Muzea" };
+    }
+}
